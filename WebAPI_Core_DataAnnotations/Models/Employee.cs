28c7aaea6c47bf29c1a49297872ab208a42fa3b8
Poornima A <apoornima1504@gmail.com>
2025-10-07 15:41:13 +0530

    updated

--- WebAPI_Core_DataAnnotations/Models/Employee.cs
+++ WebAPI_Core_DataAnnotations/Models/Employee.cs
@@ -9,14 +9,14 @@ namespace WEBAPI_Core_DataAnnotations.Models
 
         [Required(ErrorMessage = "Name is required.")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Range(18, 60, ErrorMessage = "Age must be between 18 and 60.")]
         public int Age { get; set; }
 
         [EmailAddress(ErrorMessage = "Invalid email format.")]
        [CompanyEmailAttribute]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Salary is required.")]
         [Range(10000, 200000, ErrorMessage = "Salary must be between 10,000 and 200,000.")]
